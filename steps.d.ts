64d1e5d465336fe4e39fb9711e29e08b282679e4
Ho Nguyen <ho.nguyen@wizeline.com>
2025-02-11 15:10:06 +0700

    implement Rest test case

--- steps.d.ts
+++ steps.d.ts
@@ -1,14 +1,13 @@
 /// <reference types='codeceptjs' />
 type steps_file = typeof import('./steps_file.js');
 type personOrbituaryPage = typeof import('./pages/PersonOrbituaryPage.js');
+type ChaiWrapper = import('codeceptjs-chai');
+type SQLHelper = import('./helpers/SQLHelper.js');
 
 declare namespace CodeceptJS {
-  interface SupportObject { 
-    I: I, 
-    current: any,
-    personOrbituaryPage: personOrbituaryPage }
-  interface Methods extends WebDriver {}
-  interface I extends ReturnType<steps_file> {}
+  interface SupportObject { I: I, current: any, personOrbituaryPage: personOrbituaryPage }
+  interface Methods extends WebDriver, REST, ChaiWrapper, SQLHelper {}
+  interface I extends ReturnType<steps_file>, WithTranslation<Methods> {}
   namespace Translation {
     interface Actions {}
   }
---
+++ tests/api-orbituary.test.js
@@ -0,0 +1,12 @@
+Feature ('API Orbituary Feature');
+
+let obituariesData = new DataTable(['obituaryUrl', 'expectedStatusCode', 'expectedStatusText'])
+obituariesData.add(['/us/obituaries/chicagotribune/name/virginia-gruchalski-obituary?pid=196167379', 200, 'OK'])
+obituariesData.add(['/us/obituaries/chicagotribune/name/first-last-obituary?pid=999999999', 404, 'Not Found'])
+
+Data(obituariesData).Scenario ('I can get a person orbituary record from API', async ({I, current}) => {
+    const response = await I.sendGetRequest(current.obituaryUrl)
+    // console.log('response:', response)
+    I.assertEqual(response.status, current.expectedStatusCode)
+    I.assertEqual(response.statusText, current.expectedStatusText)
+})
